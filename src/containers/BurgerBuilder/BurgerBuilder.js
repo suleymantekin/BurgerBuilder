b72a685e09a6fd53e6a1845b191d28fa1fd694ef
suleymantekin <suleymantekin@users.noreply.github.com>
2018-07-01 16:53:24 +0300

    Redirect user to checkout page when burger is built

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -63,7 +63,12 @@ class BurgerBuilder extends Component {
     // }
 
     purchaseHandler = () => {
-        this.setState({ purchasing: true });
+        if(this.props.isAthenticated){
+            this.setState({ purchasing: true });
+        }else {
+            this.props.onsSetAuthRedirectPath('/checkout');
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -94,6 +99,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disableInfo}
                         price={this.props.price}
+                        isAuth={this.props.isAthenticated}
                         purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler} />
                 </Aux>
@@ -120,7 +126,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAthenticated: state.auth.token !== null
     };
 }
 const mapDispatchedToProps = dispatch => {
@@ -128,7 +135,8 @@ const mapDispatchedToProps = dispatch => {
         onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
         onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onsSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
     }
 }
 
